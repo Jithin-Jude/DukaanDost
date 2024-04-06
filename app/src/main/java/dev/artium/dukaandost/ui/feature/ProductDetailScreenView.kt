0a59e6dec6e841ce8c2ed31f60493f2a990459d5
jithin <jithin@thecalmsleep.com>
2024-04-06 08:53:39 +0530

    ShowProductDetailsSideWise WIP

--- app/src/main/java/dev/artium/dukaandost/ui/feature/ProductDetailScreenView.kt
+++ app/src/main/java/dev/artium/dukaandost/ui/feature/ProductDetailScreenView.kt
@@ -7,12 +7,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -21,6 +23,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
@@ -155,7 +158,76 @@ fun ShowProductDetailsVertical(navController: NavHostController, selectedProduct
 
 @Composable
 fun ShowProductDetailsSideWise(navController: NavHostController, selectedProduct: ProductModel?) {
+    val configuration = LocalConfiguration.current
+    val screenHeight = configuration.screenHeightDp.dp
 
+    Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+        AsyncImage(
+            modifier = Modifier
+                .fillMaxHeight()
+                .aspectRatio(1f),
+            model = selectedProduct?.image,
+            contentDescription = null,
+            error = painterResource(R.drawable.ic_launcher_background)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        LazyColumn(
+        ) {
+            item {
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                ) {
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = selectedProduct?.title.toString(),
+                        style = Typography.bodyLarge,
+                    )
+                }
+            }
+            item {
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                ) {
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = selectedProduct?.price.toString().appendCurrencyCode(),
+                        style = Typography.titleLarge
+                    )
+                }
+            }
+            item {
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                ) {
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = selectedProduct?.rating?.rate.toString(),
+                        style = Typography.bodyLarge
+                    )
+                }
+            }
+            item {
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                ) {
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = selectedProduct?.category.toString().capitalizeFirstLetter(),
+                        style = Typography.bodyLarge
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
+            }
+        }
+    }
 }
 
 @Composable
