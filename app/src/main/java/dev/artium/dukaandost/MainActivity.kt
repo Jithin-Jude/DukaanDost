7bc594466f4cef77a5db43ae5d2d59ac74addc4c
jithin <jithin@thecalmsleep.com>
2024-04-06 12:51:50 +0530

    EmptyScreenView updates

--- app/src/main/java/dev/artium/dukaandost/MainActivity.kt
+++ app/src/main/java/dev/artium/dukaandost/MainActivity.kt
@@ -7,18 +7,24 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Refresh
 import androidx.compose.material.icons.outlined.Warning
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -124,16 +130,26 @@ class MainActivity : ComponentActivity() {
         Box(
             Modifier
                 .fillMaxSize()
-                .background(AppBackground), contentAlignment = Alignment.Center
+                .background(AppBackground)
+                .clickable {
+                    viewModel.clearFiltersAndRefresh()
+                },
+            contentAlignment = Alignment.Center
         ) {
-            Icon(imageVector = Icons.Outlined.Warning,
-                tint = DividerGrey,
-                contentDescription = "Refresh",
-                modifier = Modifier
-                    .size(48.dp)
-                    .clickable {
-                        viewModel.clearFiltersAndRefresh()
-                    })
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Icon(
+                    imageVector = Icons.Outlined.Refresh,
+                    tint = DividerGrey,
+                    contentDescription = "Refresh",
+                    modifier = Modifier
+                        .size(48.dp)
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = "No products found!\nPlease retry",
+                    textAlign = TextAlign.Center
+                )
+            }
         }
     }
 
